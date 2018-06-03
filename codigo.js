cac30f247f1d90e185f135d48152f68a3df2b66c
ArcticTopaz <36567841+ArcticTopaz@users.noreply.github.com>
2018-06-03 18:00:23 +0100

    ok

---
+++ codigo.js
@@ -0,0 +1,15 @@
+//Esta função apenas consiste em múltiplos alerts com o intuito de irritar a vitima da partida.
+function myFunction () {
+	alert("Muahahahaha agora caíste na minha armadilha!")
+	alert("Não te preocupes não é nada de mal.")
+	alert("É apenas chato ter que fechar estes alerts todos.")
+	alert("Mas prontos boa sorte!")
+	alert("Eu sei que vais conseguir!")
+	alert("Já faltou mais!")
+	alert("Será que este é o último?")
+	alert("Nah só mais um pela diversão.")
+	alert("Ok vou deixar-te ir já me diverti.")
+	alert("Ahahahahah querias não era?")
+	alert("Ainda não acabou muahahaahah!")
+	alert("Ok já acabou adeus!!")
+}
